3ee08d9ee012e2247477dd61821178ad5da07634
MichaelOliveira13 <micayl95@gmail.com>
2022-06-21 15:38:47 -0300

    First commit

---
+++ Aula74/Segundo projeto/src/Driver.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Driver{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor informe sua idade:");
+        int idade = scanner.nextInt();
+        String msg = idade >= 18 ? "Pode dirigir" : "Não pode dirigir";
+        System.out.println(msg);
+        scanner.close();
+    }
+}
---
+++ Semana-13/projeto-integrador/src/App.js
@@ -0,0 +1,12 @@
+import AddUserForm from './components/AddUserForm';
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
+}
+
+export default App;
